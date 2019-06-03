9c1630b214cd726a21eb33bf181fd931cb78e0d4
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-03 16:56:33 +0200

    Completed demo

--- 00-Setup/00-Demo/lib/demo.rb
+++ 00-Setup/00-Demo/lib/demo.rb
@@ -1,3 +1,12 @@
 def circle_area(radius)
   # TODO: Implement the `circle_area` method
+  return 0 if radius.negative?
+
+  return 3.14 * radius * radius
 end
+
+
+# This is my own test
+puts circle_area(3)
+puts circle_area(1)
+puts circle_area(0)
