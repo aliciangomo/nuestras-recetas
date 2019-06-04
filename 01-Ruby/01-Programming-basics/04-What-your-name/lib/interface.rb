d88323f167187dc3f9ad471a966a2c80aa8f2ca8
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-04 12:28:49 +0200

    Forth exercise

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -1,5 +1,19 @@
 require_relative "compute_name"
 
+puts "Please enter your first name"
+first_name = gets.chomp
+
+puts "Please enter your middle name"
+middle_name = gets.chomp
+
+puts "Please enter your last name"
+last_name = gets.chomp
+
+full_name = compute_name(first_name, middle_name, last_name)
+
+puts "Hello #{full_name}"
+
+
 # TODO: ask for the first name
 # TODO: ask for the middle name
 # TODO: ask for the last name
