178c4ead774dea702b545546833fe17ce700714f
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-04 16:31:53 +0200

    Revised

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,19 +1,3 @@
-
-# def coach_answer_enhanced(your_message)
-#   until your_message == "I am going to work right now!" || your_message == "I am going to work right now!".upcase
-#     if your_message == your_message.upcase
-#       puts "I can feel your motivation!"
-#     end
-#     if your_message[-1] == "?"
-#       puts "Silly question, get dressed and go to work!"
-#       your_message = gets.chomp
-#     else
-#       puts "I don't care, get dressed and go to work!"
-#       your_message = gets.chomp
-#     end
-#   end
-# end
-
 def coach_answer(your_message)
   if your_message == "I am going to work right now!" || your_message == "I am going to work right now!".upcase
     return ''
@@ -31,12 +15,10 @@ def coach_answer_enhanced(your_message)
     return ''
   end
   if your_message == your_message.upcase && your_message.include?("?")
-
-      return "I can feel your motivation! Silly question, get dressed and go to work!"
+    return "I can feel your motivation! Silly question, get dressed and go to work!"
   else
-      return "I can feel your motivation! I don't care, get dressed and go to work!"
+    return "I can feel your motivation! I don't care, get dressed and go to work!"
   end
-
 end
 
 
