db87e3203abda18e1e7a9f63a0cca758f0560448
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-05 00:03:11 +0200

    Colourful

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -1,3 +1,22 @@
+# def colorful?(number)
+#   # TODO: return true if the number is colorful, false otherwise
+#   a = number.digits.reverse
+#   for i in 0..a.length - 1
+#     a.push(a[i] * a[i + 1])
+#   end
+#   b = a.uniq
+#   return a.size == b.size
+# end
+
+# puts colorful?(236)
+
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
+  a = number.digits.reverse
+  for i in 0..a.length - 1
+    a.push(a[i] * a[i + 1])
+  end
+  b = a.uniq
+  return a.size == b.size
 end
+
