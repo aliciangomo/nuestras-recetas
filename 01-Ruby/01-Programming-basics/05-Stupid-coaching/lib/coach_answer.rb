ec0d9a92a6133952b7f77fca31ce610ce9feca14
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-04 14:22:48 +0200

    Fifth exercise

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,8 +1,40 @@
-def coach_answer(your_message)
-  # TODO: return coach answer to your_message
-end
+# def coach_answer(your_message)
+#   unless your_message == "I am going to work right now!"
+#     if your_message[-1] == "?"
+#       puts "Silly question, get dressed and go to work!"
+#       puts "Hello I am your coach"
+#       your_message = gets.chomp
+#     else
+#       puts "I don't care, get dressed and go to work!"
+#       puts "Hello I am your coach"
+#       your_message = gets.chomp
+#     end
+#   end
+# end
+
+# def coach_answer(your_message)
+#   until your_message == "I am going to work right now!"
+#     if your_message[-1] == "?"
+#       puts "Silly question, get dressed and go to work!"
+#       your_message = gets.chomp
+#     else
+#       puts "I don't care, get dressed and go to work!"
+#       your_message = gets.chomp
+#     end
+#   end
+# end
 
 def coach_answer_enhanced(your_message)
-  # TODO: return coach answer to your_message, with additional custom rules of yours!
+  until your_message == "I am going to work right now!" || your_message == "I am going to work right now!".upcase
+    if your_message == your_message.upcase
+      puts "I can feel your motivation!"
+    end
+    if your_message[-1] == "?"
+      puts "Silly question, get dressed and go to work!"
+      your_message = gets.chomp
+    else
+      puts "I don't care, get dressed and go to work!"
+      your_message = gets.chomp
+    end
+  end
 end
-
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,8 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+
+puts "Hello, I am your personal coach"
+user_input = gets.chomp
+
+puts coach_answer_enhanced(user_input)
+
