968747cc579f1c91bd85ef4814be36e76a04d39e
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-05 00:42:46 +0200

    corrected one rake error

--- 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
+++ 01-Ruby/01-Programming-basics/Optional-01-Colorful-Algorithm/lib/colorful.rb
@@ -12,11 +12,10 @@
 
 def colorful?(number)
   # TODO: return true if the number is colorful, false otherwise
-  a = number.digits.reverse
-  for i in 0..a.length - 1
-    a.push(a[i] * a[i + 1])
-  end
+  a = number.to_s.split('').map(&:to_i)
+  a.each { |i| a.push(a[i] * a[i + 1]) }
   b = a.uniq
   return a.size == b.size
 end
+
 
