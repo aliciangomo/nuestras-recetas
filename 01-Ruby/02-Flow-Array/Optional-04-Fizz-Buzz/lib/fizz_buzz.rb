5006cfcfef1d02e34a0eb412516b21d8b0c8f8bc
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-06 00:07:53 +0200

    optional exercise

--- 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
+++ 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
@@ -1,3 +1,21 @@
 def fizz_buzz(number)
   # TODO: return an array of integers, 'Fizz', 'Buzz' or 'FizzBuzz'
+
+  raise ArgumentError if number < 1
+
+  nums = (1..number).to_a
+  nums.map! do |num|
+    if (num % 3).zero? && (num % 5).zero?
+      "FizzBuzz"
+    elsif (num % 5).zero?
+      "Buzz"
+    elsif (num % 3).zero?
+      "Fizz"
+    else
+      num
+    end
+  end
+  return nums
 end
+
+puts fizz_buzz(18)
