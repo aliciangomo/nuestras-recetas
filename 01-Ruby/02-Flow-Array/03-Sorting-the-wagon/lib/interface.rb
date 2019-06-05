ace73f96a0a9bf49806ecddd38d7e3a42d2600e1
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-05 14:44:22 +0200

    Third exercise

--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/interface.rb
@@ -5,7 +5,19 @@ require_relative "wagon_sort"
 #       - `gets`:  http://www.ruby-doc.org/core-2.5.3/Kernel.html#method-i-gets
 #       - `chomp`: http://www.ruby-doc.org/core-2.5.3/String.html#method-i-chomp
 
+puts "Type a student name:"
+student1 = gets.chomp
+puts "Type another student name or press enter to finish:"
+student2 = gets.chomp
+puts "Type another student name or press enter to finish:"
+student3 = gets.chomp
 
 
+students = [student1, student2, student3]
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
+
+puts "Congratulations! Your Wagon has #{students.size} students :"
+hola = wagon_sort(students)
+last = hola.pop
+puts "#{hola.join(', ')} and #{last}"
--- 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Flow-Array/03-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,9 @@
+# def wagon_sort(students)
+#   # TODO: return (not print!) a copy of students, sorted alphabetically
+#   return students.sort.each {|student| puts student }
+# end
+
 def wagon_sort(students)
   # TODO: return (not print!) a copy of students, sorted alphabetically
+  return students.sort_by(&:downcase)
 end
