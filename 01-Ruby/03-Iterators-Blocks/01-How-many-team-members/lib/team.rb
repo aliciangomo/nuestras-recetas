e7c26dca3081a1d45c407572ba1eba45ebe1c972
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-06 11:25:21 +0200

    Done

--- 01-Ruby/03-Iterators-Blocks/01-How-many-team-members/lib/team.rb
+++ 01-Ruby/03-Iterators-Blocks/01-How-many-team-members/lib/team.rb
@@ -1,3 +1,6 @@
 def team_count(team_members)
   # TODO: Return the number of elements in the `team_members` array
+  return team_members.count
 end
+
+puts team_count(["Lucas", "Tom", "Clara", "Joe"])
