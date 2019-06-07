ae2a58da9c1de2691ca3369f6fb241e40d5ff0f1
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-07 10:32:19 +0200

    Update

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,17 +1,18 @@
+CALORIES = {
+  'Hamburger' => 250,
+  'Cheese Burger' => 300,
+  'Big Mac' => 540,
+  'McChicken' => 350,
+  'French Fries' => 230,
+  'Salad' => 15,
+  'Coca Cola' => 150,
+  'Sprite' => 150
+}
+
 def poor_calories_counter(burger, side, beverage)
   # TODO: return number of calories for this mcDonald order
-  calories = {
-    'Hamburger' => 250,
-    'Cheese Burger' => 300,
-    'Big Mac' => 540,
-    'McChicken' => 350,
-    'French Fries' => 230,
-    'Salad' => 15,
-    'Coca Cola' => 150,
-    'Sprite' => 150
-  }
-  return [burger, side, beverage].map { |key| calories.fetch(key) }.reduce(:+)
+  [burger, side, beverage].map { |key| CALORIES.fetch(key) }.reduce(:+)
 end
 
 
-# p poor_calories_counter("McChicken", "French Fries", "Sprite") # => 730
+p poor_calories_counter("McChicken", "French Fries", "Sprite") # => 730
