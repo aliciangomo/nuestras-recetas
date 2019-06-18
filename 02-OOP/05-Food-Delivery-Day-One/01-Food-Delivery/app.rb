7ef2b2c48b27c6b7d1539d66c1c7fa817387c3b5
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-06-18 15:01:57 +0200

    Done

--- 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app.rb
+++ 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app.rb
@@ -2,14 +2,14 @@
 # Then you can test your program with:
 #   ruby app.rb
 require_relative 'app/repositories/meal_repo'  # You need to create this file!
-require_relative 'app/controllers/meal_controller' # You need to create this file!
+require_relative 'app/controllers/meals_controller' # You need to create this file!
 require_relative 'app/router'
 
 csv_file   = File.join(__dir__, 'data/meals.csv')
 mealrepository   = MealRepository.new(csv_file)
-mealcontroller = MealController.new(mealrepository)
+mealscontroller = MealsController.new(mealrepository)
 
-router = Router.new(mealcontroller)
+router = Router.new(mealscontroller)
 
 # Start the app
 router.run
--- 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app/router.rb
+++ 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app/router.rb
@@ -1,1 +1,46 @@
 # TODO: implement the router of your app.
+class Router
+  def initialize(attributes ={})
+    @meal_controller = meal_controller
+    @running    = true
+  end
+
+  def run
+    puts "Welcome to our Menu!"
+    puts "           --           "
+
+    while @running
+      display_tasks
+      action = gets.chomp.to_i
+      print `clear`
+      route_action(action)
+    end
+  end
+
+  private
+
+  def route_action(action)
+    case action
+    when 1 then @meals_controller.list
+    when 2 then @meals_controller.add
+    when 3 then stop
+    else
+      puts "Please press 1, 2, or 3"
+    end
+  end
+
+  def stop
+    @running = false
+  end
+
+  def display_tasks
+    puts ""
+    puts "What do you want to do next?"
+    puts "1 - List all meals"
+    puts "2 - Create a new meal"
+    # puts "3 - Destroy a recipe"
+    # puts "4 - Import recipes from LetsCookFrench"
+    # puts "5 - Mark recipe as done"
+    puts "3 - Stop and exit the program"
+  end
+end
